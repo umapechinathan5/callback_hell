30bdb6e32c15924d626859d354e0ddd55ae7ae96
Uma P <155208423+umapechinathan5@users.noreply.github.com>
2024-02-26 12:57:35 +0530

    Call back uploaded

---
+++ script.js
@@ -0,0 +1,46 @@
+setTimeout(()=>{
+  const display = document.createElement("h1");
+  display.innerText = "10";
+  document.body.append(display);
+  setTimeout(()=>{
+    display.innerText = "9";
+  document.body.append(display);
+    setTimeout(()=>{
+      display.innerText = "8";
+  document.body.append(display);
+      setTimeout(()=>{
+        display.innerText = "7";
+  document.body.append(display);
+        setTimeout(()=>{
+          display.innerText = "6";
+  document.body.append(display);
+          setTimeout(()=>{
+            display.innerText = "5";
+  document.body.append(display);
+            setTimeout(()=>{
+              display.innerText = "4";
+  document.body.append(display);
+              setTimeout(()=>{
+                display.innerText = "3";
+  document.body.append(display);
+                setTimeout(()=>{
+                  display.innerText = "2";
+  document.body.append(display);
+                  setTimeout(()=>{
+                    display.innerText = "1";
+  document.body.append(display);
+                    setTimeout(()=>{
+                      display.innerText = "Happy Independence Day !!";
+  document.body.append(display);
+ 
+                    },1000)
+                  },1000)
+                },1000)
+              },1000)
+            },1000)
+          },1000)
+        },1000)
+      },1000)
+    },1000)
+  },1000)
+},1000)
